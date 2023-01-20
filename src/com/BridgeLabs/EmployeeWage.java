884405891e6dfd8b357944e19b563dbf31097cfb
unknown <gownideepu23@gmail.com>
2023-01-20 21:32:34 +0530

    added

--- src/com/BridgeLabs/EmployeeWage.java
+++ src/com/BridgeLabs/EmployeeWage.java
@@ -2,23 +2,22 @@ package com.BridgeLabs;
 
 public class EmployeeWage {
 	static final int WAGE_PER_HOUR = 20;
-	static final int FULL_DAY_HOUR = 8;
-	static final int PART_DAY_HOUR = 4;
-	static int WorkedHours=0;
+    static final int FULL_DAY_HOUR = 8;
+    static final int PART_DAY_HOUR = 4;
 	public static void main(String[] args) {
-
-		int employeeMOnthlySalary=0;		
-		int dailyWage = 0;
-		int Day=1;
 		System.out.println("Welcome to Employee Wage");
-		int employeeCheck = (int) (Math.random() * 10) % 2;
-		System.out.println(employeeCheck);
-		
-		if (employeeCheck == 1) {
-			System.out.println("Employee is Present");
-			dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-		}
-		else {
+        int employeeCheck = (int) (Math.random() * 10) % 3;
+        System.out.println(employeeCheck);
+        int dailyWage = 0;
+        if (employeeCheck == 1) {
+            System.out.println("Employee is Present");
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        }
+        else if (employeeCheck==2) {
+        	System.out.println("Employee is Present as part time");
+            dailyWage = WAGE_PER_HOUR * PART_DAY_HOUR;
+        }
+        else {
             System.out.println("Employee is Absent");
         }
         System.out.println("Daily wage => " + dailyWage);
